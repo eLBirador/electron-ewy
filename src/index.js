5ff1398349199be4dde73c9a2f745999c16b17b9
Gerald Villacarlos <gerald@coassemble.com>
2021-06-12 22:25:11 +1000

    MVP Baby!

--- src/index.js
+++ src/index.js
@@ -24,6 +24,8 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
+  mainWindow.setAlwaysOnTop(true, 'screen');
+
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
 };
--- src/preload.js
+++ src/preload.js
@@ -11,4 +11,48 @@ window.addEventListener('DOMContentLoaded', () => {
     for (const dependency of ['chrome', 'node', 'electron']) {
         replaceText(`${dependency}-version`, process.versions[dependency])
     }
+
+    var questions = [
+        {q:"What is 1+1?", a:"2"},
+        {q:"What is 2+2?", a:"4"},
+        {q:"What is 10+5?", a:"15"},
+        {q:"What is 20-1?", a:"19"}
+    ];
+    const item = questions[Math.floor(Math.random()*questions.length)];
+
+    let q = document.getElementById("question");
+    q.innerHTML = item.q;
+
+    let btn = document.getElementById("submit");
+    let status = document.getElementById("status");
+
+    btn.addEventListener('click', () => {
+        let answer = document.getElementById("answer");
+        console.log(item);
+        if (answer != "") {
+            if (answer.value == item.a) {
+                status.innerHTML = "";
+                var correct_msgs = [
+                    {s:"Great Job!"},
+                    {s:"Wow, Youre a champ!"},
+                ];
+                var msg = correct_msgs[Math.floor(Math.random()*correct_msgs.length)];
+                alert( msg.s + ', See you later!');
+                window.close();
+            } else {
+                var statuses = [
+                    {s:"Oopsie daisy, Try again!"},
+                    {s:"Nice Try!, Think for a sec and try again!"},
+                ];
+                var msg = statuses[Math.floor(Math.random()*statuses.length)];
+
+                answer.value = "";
+                status.innerHTML = msg.s;
+                answer.blur();
+            }
+        } else {
+            alert('Call Daddy!');
+        }
+    });
+    
 })
